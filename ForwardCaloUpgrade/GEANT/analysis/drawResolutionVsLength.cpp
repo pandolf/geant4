dc90bd7b3f34edb330b15195e857db517d080e84
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2013-11-07 12:16:17 +0100

    ready for the rootfiles now

--- ForwardCaloUpgrade/GEANT/analysis/drawResolutionVsLength.cpp
+++ ForwardCaloUpgrade/GEANT/analysis/drawResolutionVsLength.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 
 #include "CommonTools/DrawBase.h"
@@ -6,7 +7,8 @@
 #include "TCanvas.h"
 
 
-TH1D* get_histo_vs_length( const std::string& batchProd, float activeLayerThickness, float absorberLayerThickness, int nLayers );
+std::pair<TH1D*,TH1D*> get_histos_vs_length( const std::string& batchProd, float activeLayerThickness, float absorberLayerThickness, int nLayers );
+void drawComparison( DrawBase* db, const std::string& saveName, const std::string& yAxisName, TH1D* h1_act5_abs5, TH1D* h1_act5_abs2, TH1D* h1_act10_abs5, TH1D* h1_act10_abs2 );
 
 
 
@@ -15,26 +17,37 @@ int main() {
 
   DrawBase* db = new DrawBase("db");
 
+  std::string outputdir = "plots";
+  db->set_outputdir(outputdir);
+  std::string mkdir_command = "mkdir -p " + outputdir;
+  system(mkdir_command.c_str());
+
   std::string batchProd = "provaNEW1";
 
   int nLayers = 30;
-  float activeLayerThickness = 5.;
-  float absorberLayerThickness = 5.;
 
-  TH1D* h1_resolution_vs_length_act5_abs5 =  get_histo_vs_length( batchProd, activeLayerThickness, absorberLayerThickness, nLayers );
+  std::pair<TH1D*,TH1D*> pair_act5_abs5 =  get_histos_vs_length( batchProd, 5., 5., nLayers );
+  std::pair<TH1D*,TH1D*> pair_act5_abs2 =  get_histos_vs_length( batchProd, 5., 2., nLayers );
+  std::pair<TH1D*,TH1D*> pair_act10_abs5 =  get_histos_vs_length( batchProd, 10., 5., nLayers );
+  std::pair<TH1D*,TH1D*> pair_act10_abs2 =  get_histos_vs_length( batchProd, 10., 2., nLayers );
 
+  drawComparison( db, "reso", "Resolution", pair_act5_abs5.second, pair_act5_abs2.second, pair_act10_abs5.second, pair_act10_abs2.second );
 
   return 0;
 
 }
 
 
-TH1D* get_histo_vs_length( const std::string& batchProd, float activeLayerThickness, float absorberLayerThickness, int nLayers ) {
+std::pair<TH1D*,TH1D*> get_histos_vs_length( const std::string& batchProd, float activeLayerThickness, float absorberLayerThickness, int nLayers ) {
 
 
-  char histoName[200];
-  sprintf( histoName, "resolution_vs_length_act%.0f_abs%.0f", activeLayerThickness, absorberLayerThickness );
-  TH1D* h1 = new TH1D( histoName, "", nLayers, 0., (float)nLayers );
+  char histoName_sf[200];
+  sprintf( histoName_sf, "sf_vs_length_act%.0f_abs%.0f", activeLayerThickness, absorberLayerThickness );
+  TH1D* h1_sf = new TH1D( histoName_sf, "", nLayers, 0., (float)nLayers );
+
+  char histoName_reso[200];
+  sprintf( histoName_reso, "resolution_vs_length_act%.0f_abs%.0f", activeLayerThickness, absorberLayerThickness );
+  TH1D* h1_reso = new TH1D( histoName_reso, "", nLayers, 0., (float)nLayers );
 
 
   for( unsigned iLayer=1; iLayer<=nLayers; ++iLayer ) {
@@ -50,13 +63,74 @@ TH1D* get_histo_vs_length( const std::string& batchProd, float activeLayerThickn
 
     float resolution = sf_rms/sf_mean;
 
-    h1->SetBinContent( iLayer, resolution );
+    h1_sf->SetBinContent( iLayer, sf_mean );
+    h1_reso->SetBinContent( iLayer, resolution );
 
     file->Close();
 
   }
 
+  std::pair<TH1D*,TH1D*> returnPair;
+  returnPair.first = h1_sf;
+  returnPair.second = h1_reso;
   
-  return h1;
+  return returnPair;
+
+}
+
+
+
+void drawComparison( DrawBase* db, const std::string& saveName, const std::string& yAxisName, TH1D* h1_act5_abs5, TH1D* h1_act5_abs2, TH1D* h1_act10_abs5, TH1D* h1_act10_abs2 ) {
+
+
+  h1_act5_abs5->SetMarkerStyle(20);
+  h1_act5_abs5->SetMarkerColor(46);
+  h1_act5_abs5->SetMarkerSize(1.3);
+
+  h1_act5_abs2->SetMarkerStyle(21);
+  h1_act5_abs2->SetMarkerColor(kRed+1);
+  h1_act5_abs2->SetMarkerSize(1.3);
+
+  h1_act10_abs5->SetMarkerStyle(24);
+  h1_act10_abs5->SetMarkerColor(29);
+  h1_act10_abs5->SetMarkerSize(1.3);
+
+  h1_act10_abs2->SetMarkerStyle(25);
+  h1_act10_abs2->SetMarkerColor(13);
+  h1_act10_abs2->SetMarkerSize(1.3);
+
+
+  float xMax = h1_act5_abs5->GetXaxis()->GetXmax();
+  float yMax = h1_act5_abs5->GetMaximum();
+  if( h1_act5_abs2->GetMaximum()>yMax ) yMax = h1_act5_abs2->GetMaximum();
+  if( h1_act10_abs5->GetMaximum()>yMax ) yMax = h1_act10_abs5->GetMaximum();
+  if( h1_act10_abs2->GetMaximum()>yMax ) yMax = h1_act10_abs2->GetMaximum();
+  yMax *= 1.1;
+  
+
+  TCanvas* c1 = new TCanvas("c1", "", 600, 600);
+  c1->cd();
+
+  TH2D* h2_axes = new TH2D("axes", "", 10, 0., xMax, 10, 0., yMax );
+  h2_axes->SetXTitle("Number of Layers");
+  h2_axes->SetYTitle(yAxisName.c_str());
+
+  h2_axes->Draw();
+
+  h1_act5_abs5->Draw("P same");
+  h1_act5_abs2->Draw("P same");
+  h1_act10_abs5->Draw("P same");
+  h1_act10_abs2->Draw("P same");
+
+
+  std::string canvasName = db->get_outputdir() + "/" + saveName +"_vs_length";
+  std::string canvasName_eps = canvasName+".eps";
+  std::string canvasName_png = canvasName+".png";
+
+  c1->SaveAs(canvasName_eps.c_str());
+  c1->SaveAs(canvasName_png.c_str());
+
+  delete c1;
+  delete h2_axes;
 
 }
