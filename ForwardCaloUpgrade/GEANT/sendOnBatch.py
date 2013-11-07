caac5bcc1ae0f6f86024d24d052fe6482280e3f3
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2013-11-07 12:59:36 +0100

    works, finally

--- ForwardCaloUpgrade/GEANT/sendOnBatch.py
+++ ForwardCaloUpgrade/GEANT/sendOnBatch.py
@@ -20,11 +20,13 @@ queue = "8nh"
 
 diskoutputdir = "/cmsrm/pc24_2/pandolf/geant/EcalEndcaps/750MeV/" + batchName
 
-dir = "batchDir_750MeV_" + batchName
+dir = "/afs/cern.ch/work/p/pandolf/geant4/ForwardCaloUpgrade/GEANT/batchOutput_750MeV_" + batchName
 os.system("mkdir -p "+dir)
 os.system("mkdir -p "+dir+"/log/")
 os.system("mkdir -p "+dir+"/input/")
 os.system("mkdir -p "+dir+"/src/")
+os.system("mkdir -p "+dir+"/rootfiles/")
+os.system("mkdir -p "+dir+"/tables/")
 
 
 
@@ -41,15 +43,20 @@ suffix = "n"+ str(nLayers) + "_act" + str(activeLayerThickness) + "_abs" + str(a
 # prepare the script to run
 outputname = dir+"/src/submit_n" + str(nLayers) + "_act" + str(activeLayerThickness) + "_abs" + str(absorberLayerThickness) + ".src"
 outputfile = open(outputname,'w')
-outputfile.write('#!/bin/tcsh\n')
-outputfile.write('setenv LANGUAGE C\n')
-outputfile.write('setenv LC_ALL C\n')
-outputfile.write('cd /afs/cern.ch/work/p/pandolf/geant4/ForwardCaloUpgrade/GEANT/; source newconfig.csh ; cd -\n')
-outputfile.write('cd $WORKDIR\n')
-outputfile.write("./scripts/simulation_750MeV_batch.sh full " + str(nLayers)+ " " + str(activeLayerThickness) + " " + str(absorberLayerThickness) + "\n")
-outputfile.write('ls rootfiles/samplinghistos_' + suffix + '*.root | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm24:'+diskoutputdir+'/rootfiles/{}\n') 
-outputfile.write('ls tables/parameters_' + suffix + '*.dat | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm24:'+diskoutputdir+'/tables/{}\n') 
+outputfile.write('#!/bin/bash\n')
+outputfile.write('export LANGUAGE=C\n')
+outputfile.write('export LC_ALL=C\n')
+outputfile.write('cd /afs/cern.ch/work/p/pandolf/geant4/ForwardCaloUpgrade/GEANT/\n')
+outputfile.write('source newconfig.sh\n')
+#outputfile.write('source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/bin/thisroot.sh\n')
+#outputfile.write('cd -\n')
+#outputfile.write('cd $WORKDIR\n')
+outputfile.write( "bash "+ pwd + "/scripts/simulation_750MeV_batch.sh full " + str(nLayers)+ " " + str(activeLayerThickness) + " " + str(absorberLayerThickness) + "\n")
+#outputfile.write('ls rootfiles/samplinghistos_' + suffix + '*.root | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm24:'+diskoutputdir+'/rootfiles/{}\n') 
+#outputfile.write('ls tables/parameters_' + suffix + '*.dat | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm24:'+diskoutputdir+'/tables/{}\n') 
+outputfile.write('ls rootfiles/samplinghistos_' + suffix + '*.root | xargs -i cp {} '+dir+'/{}\n') 
+outputfile.write('ls tables/parameters_' + suffix + '*.dat | xargs -i cp {} '+dir+'/{}\n') 
 outputfile.close
-os.system("echo bsub -q "+queue+" -o "+dir+"/log/log_"+suffix+".log source "+pwd+"/"+outputname)
-os.system("bsub -q "+queue+" -o "+dir+"/log/log_"+suffix+".log source "+pwd+"/"+outputname+" -copyInput= "+suffix)
+os.system("echo bsub -q "+queue+" -o "+dir+"/log/log_"+suffix+".log source "+outputname)
+os.system("bsub -q "+queue+" -o "+dir+"/log/log_"+suffix+".log source "+outputname+" -copyInput= "+suffix)
 
