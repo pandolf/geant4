f58ea046643cdcdc3b6daf25df652fef9e8721a0
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2013-11-07 11:01:14 +0100

    most probably fixed the paths now with manual setenvs

--- ForwardCaloUpgrade/GEANT/newconfig.csh
+++ ForwardCaloUpgrade/GEANT/newconfig.csh
@@ -32,11 +32,31 @@ else
  echo "No LD_LIBRARY_PATH at all - check $LD_LIBRARY_PATH"
 endif
 
- setenv MyGeant ~/geant4/9.4.p02/x86_64-slc6-gcc46-opt/bin/Linux-g++
+setenv MyGeant ~/geant4/9.4.p02/x86_64-slc6-gcc46-opt/bin/Linux-g++
 
- echo " "
- echo "Your geant4 directory (alias MyGeant) with executable fcalor:"
- echo "$MyGeant"
- echo " "
+#echo " "
+#echo "Your geant4 directory (alias MyGeant) with executable fcalor:"
+#echo "$MyGeant"
+#echo " "
 
- source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/bin/thisroot.csh
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/bin/thisroot.csh
+
+setenv ROOTSYS /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root
+setenv HEPMC_DIR /afs/cern.ch/sw/lcg/external/HepMC/2.06.08/x86_64-slc6-gcc46-opt
+setenv MyGeant /afs/cern.ch/user/p/pandolf/geant4/9.4.p02/x86_64-slc6-gcc46-opt/bin/Linux-g++
+setenv DYLD_LIBRARY_PATH /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib:
+setenv SHLIB_PATH /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib:
+setenv LIBPATH /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib:
+setenv PYTHONPATH /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib:
+#setenv BASEDIR /afs/cern.ch/work/p/pandolf/geant4/
+#setenv MANPATH /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/man:/afs/cern.ch/cms/share/man:/afs/cern.ch/cms/share/man:
+#setenv LD_LIBRARY_PATH /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib
+
+
+
+
+if( $?CMSSW_BASE ) then
+  setenv BASEDIR $CMSSW_BASE/src
+else
+  setenv BASEDIR /afs/cern.ch/work/p/pandolf/geant4/
+endif
--- ForwardCaloUpgrade/GEANT/newconfig.sh
+++ ForwardCaloUpgrade/GEANT/newconfig.sh
@@ -2,17 +2,17 @@
 
 source /afs/cern.ch/sw/lcg/external/gcc/4.6.3/x86_64-slc6-gcc46-opt/setup.sh
 
-setenv G4BASE /afs/cern.ch/sw/lcg/external/geant4
+export G4BASE=/afs/cern.ch/sw/lcg/external/geant4
 
-source $G4BASE/9.6.p02/x86_64-slc6-gcc46-opt/GNUmake-setup.csh
+source $G4BASE/9.6.p02/x86_64-slc6-gcc46-opt/GNUmake-setup.sh
 
 
-setenv ROOTSYS /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root
+export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root
 echo "  "
 echo "You have choosen the next ROOTSYS version:"
 echo "$ROOTSYS"
 
-setenv HEPMC_DIR /afs/cern.ch/sw/lcg/external/HepMC/2.06.08/x86_64-slc6-gcc46-opt
+export HEPMC_DIR=/afs/cern.ch/sw/lcg/external/HepMC/2.06.08/x86_64-slc6-gcc46-opt
 
 echo " " 
 echo "You have choosen the next HepMC version:" 
@@ -20,23 +20,35 @@ echo "$HEPMC_DIR"
 
 
 
-if( $?LD_LIBRARY_PATH ) then
-  setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:$HEPMC_DIR/lib
+if [ $?LD_LIBRARY_PATH ] ; then
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$HEPMC_DIR/lib
+else
+  echo "No LD_LIBRARY_PATH at all - check $LD_LIBRARY_PATH"
+fi
+
+if [ $?LD_LIBRARY_PATH ] ; then
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$ROOTSYS/lib
 else
  echo "No LD_LIBRARY_PATH at all - check $LD_LIBRARY_PATH"
-endif
+fi
 
-if( $?LD_LIBRARY_PATH ) then
-  setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:$ROOTSYS/lib
-else
- echo "No LD_LIBRARY_PATH at all - check $LD_LIBRARY_PATH"
-endif
+export MyGeant=~/geant4/9.4.p02/x86_64-slc6-gcc46-opt/bin/Linux-g++
 
- setenv MyGeant ~/geant4/9.4.p02/x86_64-slc6-gcc46-opt/bin/Linux-g++
+echo " "
+echo "Your geant4 directory (alias MyGeant) with executable fcalor:"
+echo "$MyGeant"
+echo " "
 
- echo " "
- echo "Your geant4 directory (alias MyGeant) with executable fcalor:"
- echo "$MyGeant"
- echo " "
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/bin/thisroot.sh
 
- source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/bin/thisroot.csh
+export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root
+export HEPMC_DIR=/afs/cern.ch/sw/lcg/external/HepMC/2.06.08/x86_64-slc6-gcc46-opt
+export MyGeant=/afs/cern.ch/user/p/pandolf/geant4/9.4.p02/x86_64-slc6-gcc46-opt/bin/Linux-g++
+export DYLD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib:
+export SHLIB_PATH=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib:
+export LIBPATH=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib:
+export PYTHONPATH=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.08/x86_64-slc6-gcc46-opt/root/lib:
+export BASEDIR=/afs/cern.ch/work/p/pandolf/geant4/
+
+
+
