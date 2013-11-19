8a2cee288e5de3b5d61c17bf832621e6833480e5
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2013-11-19 09:58:13 +0100

    now project from Cell tree works

--- ForwardCaloUpgrade/GEANT/analysis/drawResolutionVsLength.cpp
+++ ForwardCaloUpgrade/GEANT/analysis/drawResolutionVsLength.cpp
@@ -7,15 +7,16 @@
 #include "TCanvas.h"
 
 
-std::pair<TH1D*,TH1D*> get_histos_vs_length( const std::string& batchProd, float activeLayerThickness, float absorberLayerThickness, int nLayers );
-void drawComparison( DrawBase* db, const std::string& varName, const std::string& yAxisName, TH1D* h1_1, TH1D* h1_2, TH1D* h1_3, TH1D* h1_4 );
+void drawStuffForOneVariable( DrawBase* db, const std::string& varName, const std::string& axisName, const std::string& units, const std::string& batchProd, int nLayers );
+std::pair<TH1D*,TH1D*> get_histos_vs_length( const std::string& varName, const std::string& batchProd, float activeLayerThickness, float absorberLayerThickness, int nLayers );
+void drawComparison( DrawBase* db, const std::string& varName, const std::string& yAxisName, const std::string& units, TH1D* h1_1, TH1D* h1_2, TH1D* h1_3, TH1D* h1_4 );
 
 
 
 int main() {
 
 
-  std::string batchProd = "2p5k_v1";
+  std::string batchProd = "2p5k_v4_withTemp";
   //std::string batchProd = "5k_v4";
 
   DrawBase* db = new DrawBase("db");
@@ -28,37 +29,49 @@ int main() {
 
   int nLayers = 30;
 
-  std::pair<TH1D*,TH1D*> pair_act5_abs5 =  get_histos_vs_length( batchProd, 5., 5., nLayers );
-  std::pair<TH1D*,TH1D*> pair_act5_abs2 =  get_histos_vs_length( batchProd, 5., 2., nLayers );
-  std::pair<TH1D*,TH1D*> pair_act10_abs5 =  get_histos_vs_length( batchProd, 10., 5., nLayers );
-  std::pair<TH1D*,TH1D*> pair_act10_abs2 =  get_histos_vs_length( batchProd, 10., 2., nLayers );
-
-  drawComparison( db, "resolution", "Resolution", pair_act5_abs5.second, pair_act5_abs2.second, pair_act10_abs5.second, pair_act10_abs2.second );
-  drawComparison( db, "sf", "Sampling Fraction", pair_act5_abs5.first, pair_act5_abs2.first, pair_act10_abs5.first, pair_act10_abs2.first );
+  drawStuffForOneVariable( db, "sf",         "Sampling Fraction", "", batchProd, nLayers );
+  drawStuffForOneVariable( db, "ecal",       "ECAL Energy", "MeV", batchProd, nLayers );
+  drawStuffForOneVariable( db, "e_dep",      "ECAL Energy", "MeV", batchProd, nLayers );
 
   return 0;
 
 }
 
 
-std::pair<TH1D*,TH1D*> get_histos_vs_length( const std::string& batchProd, float activeLayerThickness, float absorberLayerThickness, int nLayers ) {
+void drawStuffForOneVariable( DrawBase* db, const std::string& varName, const std::string& axisName, const std::string& units, const std::string& batchProd, int nLayers ) {
+
+  std::pair<TH1D*,TH1D*> pair_act5_abs5  =  get_histos_vs_length( varName, batchProd, 5., 5., nLayers );
+  std::pair<TH1D*,TH1D*> pair_act5_abs2  =  get_histos_vs_length( varName, batchProd, 5., 2., nLayers );
+  std::pair<TH1D*,TH1D*> pair_act10_abs5 =  get_histos_vs_length( varName, batchProd, 10., 5., nLayers );
+  std::pair<TH1D*,TH1D*> pair_act10_abs2 =  get_histos_vs_length( varName, batchProd, 10., 2., nLayers );
+
+  drawComparison( db, varName+"res", axisName + " Resolution",    "", pair_act5_abs5.second, pair_act5_abs2.second, pair_act10_abs5.second, pair_act10_abs2.second );
+  drawComparison( db, varName,       axisName,                 units, pair_act5_abs5.first,  pair_act5_abs2.first,  pair_act10_abs5.first,  pair_act10_abs2.first );
+
+}
+
+
+std::pair<TH1D*,TH1D*> get_histos_vs_length( const std::string& varName, const std::string& batchProd, float activeLayerThickness, float absorberLayerThickness, int nLayers ) {
 
 
   char histoName_sf[200];
-  sprintf( histoName_sf, "sf_vs_length_act%.0f_abs%.0f", activeLayerThickness, absorberLayerThickness );
+  sprintf( histoName_sf, "%s_vs_length_act%.0f_abs%.0f", varName.c_str(), activeLayerThickness, absorberLayerThickness );
   TH1D* h1_sf = new TH1D( histoName_sf, "", nLayers, 0.5, (float)nLayers+0.5 );
 
   char histoName_reso[200];
-  sprintf( histoName_reso, "resolution_vs_length_act%.0f_abs%.0f", activeLayerThickness, absorberLayerThickness );
+  sprintf( histoName_reso, "%sres_vs_length_act%.0f_abs%.0f", varName.c_str(), activeLayerThickness, absorberLayerThickness );
   TH1D* h1_reso = new TH1D( histoName_reso, "", nLayers, 0.5, (float)nLayers+0.5 );
+
+  std::string histoName_get = "h_" + varName + "_config0_750MeV";
+
 
 
   for( unsigned iLayer=1; iLayer<nLayers; ++iLayer ) {
 
-    if( activeLayerThickness==10. && absorberLayerThickness==5. && iLayer>26 ) continue;
+    if( activeLayerThickness==10. && absorberLayerThickness==5. && iLayer>26 ) continue; // EE is too long (>400 cm)
 
-    char fileName[200];
-    sprintf( fileName, "../batchOutput_750MeV_%s/rootfiles/samplinghistos_n%d_act%.0f_abs%.0f.root", batchProd.c_str(), iLayer, activeLayerThickness, absorberLayerThickness );
+    char fileName[500];
+    sprintf( fileName, "../batchOutput_750MeV_%s/rootfiles/samplinghistos_n%d_act%.0f_abs%.0f_trasv20.root", batchProd.c_str(), iLayer, activeLayerThickness, absorberLayerThickness );
     TFile* file = TFile::Open( fileName );
 
     if( file==0 ) {
@@ -68,8 +81,39 @@ std::pair<TH1D*,TH1D*> get_histos_vs_length( const std::string& batchProd, float
       continue;
     }
 
-    TH1D* h1_sf_tmp = (TH1D*)file->Get("h_sf_config0_750MeV");
-    
+    TH1D* h1_sf_tmp = (TH1D*)file->Get(histoName_get.c_str());
+
+    if( h1_sf_tmp==0 ) {
+ 
+      TH1F::AddDirectory(kTRUE);
+
+      if( iLayer==1 ) 
+        std::cout << "-> Didn't find histogram: '" << histoName_get << ". Will go for the Cell tree." << std::endl;
+
+      char fileName_temp[500];
+      sprintf( fileName_temp, "../batchOutput_750MeV_%s/rootfiles/temp/temp_n%d_act%.0f_abs%.0f_trasv20.root", batchProd.c_str(), iLayer, activeLayerThickness, absorberLayerThickness );
+      TFile* file_temp = TFile::Open( fileName_temp );
+
+      TTree* tree_cell = (TTree*)file_temp->Get("Cell");
+
+      int n_cells;
+      tree_cell->SetBranchAddress( "n_cells", &n_cells);
+      tree_cell->GetEntry(1);
+
+      std::string treeVarName = varName + "[0]";
+      for( unsigned icell=1; icell<n_cells; ++icell ) { //add for all cells
+        char anotherCellName[100];
+        sprintf( anotherCellName, "+%s[%d]", varName.c_str(), icell );
+        std::string anotherCellName_str(anotherCellName);
+        treeVarName += anotherCellName_str;
+      }
+
+      h1_sf_tmp = new TH1D("sf_tmp", "", 200, 0., 750.); //eMax = 750 mev
+      
+      tree_cell->Project( "sf_tmp", treeVarName.c_str() );
+
+    }
+
     float sf_mean = h1_sf_tmp->GetMean();
     float sf_rms  = h1_sf_tmp->GetRMS();
 
@@ -101,7 +145,10 @@ std::pair<TH1D*,TH1D*> get_histos_vs_length( const std::string& batchProd, float
 
 
 
-void drawComparison( DrawBase* db, const std::string& varName, const std::string& yAxisName, TH1D* h1_1, TH1D* h1_2, TH1D* h1_3, TH1D* h1_4 ) {
+void drawComparison( DrawBase* db, const std::string& varName, const std::string& yAxisName, const std::string& units, TH1D* h1_1, TH1D* h1_2, TH1D* h1_3, TH1D* h1_4 ) {
+
+  TString yAxisName_tstr(yAxisName);
+  bool isResolution = yAxisName_tstr.Contains("Resolution");
 
 
   const char* name1 = h1_1->GetName();
@@ -145,16 +192,21 @@ void drawComparison( DrawBase* db, const std::string& varName, const std::string
   if( h1_2->GetMaximum() >yMax ) yMax = h1_2 ->GetMaximum();
   if( h1_3->GetMaximum()>yMax )  yMax = h1_3->GetMaximum();
   if( h1_4->GetMaximum()>yMax )  yMax = h1_4->GetMaximum();
-  yMax *= 1.6;
+  float scaleFactor = 1.5;
+  if( isResolution ) scaleFactor = 1.1;
+  yMax *= scaleFactor;
   
+  if(varName=="ecal"||varName=="e_dep") yMax = 900.;
 
   TCanvas* c1 = new TCanvas("c1", "", 600, 600);
   c1->cd();
 
   TH2D* h2_axes = new TH2D("axes", "", 10, 0., xMax, 10, 0., yMax );
   h2_axes->SetXTitle("Number of Layers");
-  h2_axes->SetYTitle(yAxisName.c_str());
-
+  std::string totalAxisName = yAxisName;
+  if( units!="" ) totalAxisName = totalAxisName + " [" + units + "]";
+  h2_axes->SetYTitle(totalAxisName.c_str());
+  
   h2_axes->Draw();
 
   TLegend* legend = new TLegend( 0.58, 0.63, 0.9, 0.9, "CeF_{3} / Lead" );
@@ -197,6 +249,7 @@ void drawComparison( DrawBase* db, const std::string& varName, const std::string
   h1_3->Draw("P same");
   h1_4->Draw("P same");
 
+  gPad->RedrawAxis();
 
   std::string canvasName = db->get_outputdir() + "/" + varName +"_vs_length";
   std::string canvasName_eps = canvasName+".eps";
