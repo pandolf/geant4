75414233ee21878e6c1fe8223cb1e0deaf61cc96
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-03-17 12:19:02 +0100

    now switched to simulation_batch2, completely automatic

--- ForwardCaloUpgrade/GEANT/sendOnBatch2.py
+++ ForwardCaloUpgrade/GEANT/sendOnBatch2.py
@@ -10,7 +10,7 @@ from optparse import OptionParser
 def get_string(x):
   thestring = str(int(x))
   if ( math.modf(x)[0] > 0. ) :
-    thestring = str(int(math.modf(x)[1]) ) + "p" + str(int(10.*(math.modf(x)[0])) )
+    thestring = str(int(math.modf(x)[1]) ) + "p" + str(int( round(10.*(math.modf(x)[0]), 2) ) )
   return thestring
 
 
@@ -27,6 +27,8 @@ parser.add_option("","--abs",dest="absorberLayerThickness",default=10)
 parser.add_option("","--transv",dest="transverseCellSize", default=20)
 parser.add_option("-i","--impact",dest="impactPosition", default=0)
 parser.add_option("-e","--energy",dest="energy", default=0.75)
+parser.add_option("","--actType",dest="activeType",default="CeF3")
+parser.add_option("","--nEvents",dest="nEvents",default="5000")
 
 (options,args)=parser.parse_args()
 
@@ -40,9 +42,11 @@ parser.add_option("-e","--energy",dest="energy", default=0.75)
 
 dir = "/afs/cern.ch/work/p/pandolf/geant4/ForwardCaloUpgrade/GEANT/batchOutput_" + str(options.batchName)
 if ( options.energy < 1. ) :
-  dir = dir + "/" + str((int)(1000*options.energy)) + "MeV/"
+  dir = dir + "/" + str((int)(1000*options.energy)) + "MeV"
 else :
-  dir = dir + "/" + str((int)(options.energy)) + "GeV/"
+  dir = dir + "/" + str((int)(options.energy)) + "GeV"
+if( options.activeType!="CeF3" ) :
+  dir = dir + "/" + str(options.activeType) 
 
 
 print("Creating directory: "+dir)
@@ -62,12 +66,14 @@ act = float(options.activeLayerThickness)
 abs = float(options.absorberLayerThickness)
 trasv = float(options.transverseCellSize)
 impact = float(options.impactPosition)
+activeType = str(options.activeType)
+
 
 # convert floats to usable strings:
 act_str = get_string(act)
-abs_str = get_string(act)
-trasv_str = get_string(act)
-impact_str = get_string(act)
+abs_str = get_string(abs)
+trasv_str = get_string(trasv)
+impact_str = get_string(impact)
 
 
 
@@ -77,6 +83,18 @@ if (int(options.impactPosition) > 0) :
     suffix = suffix + "_impact" + impact_str
 
 
+# prepare script
+os.system("cp scripts/simulation_batch2.sh scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/simulation_750MeV_batch/s/simulation_750MeV_batch/simulation_750MeV_local/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/energ=/s/10/" + str(options.energy) + "/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/nlayers=/s/10/" + str(options.nLayers) + "/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/sensthick=/s/10/" + str(act) + "/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/absthick=/s/3/" + str(abs) + "/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/transverseCellSize=/s/25/" + str(trasv) + "/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/impact=/s/25/" + str(impact) + "/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/sensitiv=/s/CeF3/" + str(activeType) + "/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("sed -i'' \"/events=/s/5000/" + str(options.nEvents) + "/g\" scripts/simulation_batch2_tmp.sh\n")
+os.system("chmod +x ./scripts/simulation_batch2_tmp.sh\n")
 
 # prepare the script to run
 outputname = dir+"/src/submit_" + suffix + ".src"
@@ -86,7 +104,7 @@ outputfile.write('export LANGUAGE=C\n')
 outputfile.write('export LC_ALL=C\n')
 outputfile.write('cd /afs/cern.ch/work/p/pandolf/geant4/ForwardCaloUpgrade/GEANT/\n')
 outputfile.write('source newconfig.sh\n')
-outputfile.write("bash "+ pwd + "/scripts/simulation_batch.sh full " +str(options.energy) + " " + str(options.nLayers)+ " " + str(options.activeLayerThickness) + " " + str(options.absorberLayerThickness) + " " + str(options.transverseCellSize) + " " + str(options.impactPosition) + "\n")
+outputfile.write("bash "+ pwd + "/scripts/simulation_batch2_tmp.sh\n")
 outputfile.write('ls rootfiles/samplinghistos_' + suffix + '*.root | xargs -i mv {} '+dir+'/{}\n') 
 outputfile.write('ls rootfiles/temp/temp_' + suffix + '*.root | xargs -i mv {} '+dir+'/{}\n') 
 outputfile.write('ls tables/parameters_' + suffix + '*.dat | xargs -i mv {} '+dir+'/{}\n') 
@@ -94,3 +112,5 @@ outputfile.write('ls inputfiles/tempinput_' + suffix + '*.in | xargs -i mv {} '+
 outputfile.close
 os.system("echo bsub -q "+options.queue+" -o "+dir+"/log/log_"+suffix+".log source "+outputname)
 os.system("bsub -q "+options.queue+" -o "+dir+"/log/log_"+suffix+".log source "+outputname+" -copyInput= "+suffix)
+
+os.system("mv ./scripts/simulation_batch2_tmp.sh " + dir + "\n")
